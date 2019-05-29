ab0053d53d602f17237970b9a49d296548a03f56
junjie-lean <40194410+junjie-lean@users.noreply.github.com>
2019-05-29 18:17:34 +0800

    commit

---
+++ code/task.js
@@ -0,0 +1,52 @@
+console.log("1");//task1
+setTimeout(function() {//task2
+  console.log("2");
+  process.nextTick(function() {
+    console.log("3");
+  });
+  new Promise(function(resolve) {
+    console.log("4");
+    resolve();
+  }).then(function() {
+    console.log("5");
+  });
+},4);
+
+new Promise(function(resolve) {//task3
+  console.log("6");
+  resolve();
+}).then(function() {
+  console.log("7");
+});
+process.nextTick(function() { //task4
+  console.log("8");
+});
+setImmediate(() => {//task5
+  console.info("9");
+});
+new Promise(function(resolve) { //task6
+  console.log("10");
+  resolve();
+}).then(function() {
+  console.log("11");
+});
+
+setTimeout(function() { //task7
+  console.log("12");
+  setImmediate(() => {
+    console.info("13");
+  });
+  process.nextTick(function() {
+    console.log("14");
+  });
+  new Promise(function(resolve) {
+    console.log("15");
+    resolve();
+  }).then(function() {
+    console.log("16");
+  });
+});
+
+process.nextTick(function() {//task8
+  console.log("17");
+});
---
+++ 杂项/JavaScript的Event Loop.md
@@ -0,0 +1,9 @@
+### Event Loop
+
+JavaScript的Event Loop是一个执行模型，在不同的地方有不同的实现，也就是说，在浏览器里的event loop实现和node.js中的event loop实现是不同的。
+
+
+
+
+
+相关文档：[这里](https://cnodejs.org/topic/5a9108d78d6e16e56bb80882#5a98d9a2ce1c90bc44c445af)。
